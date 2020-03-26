1b02bbbbeaa0273b15db64dbf04f3762032bcdb7
aydinma97 <muhammedaliaydin97@gmail.com>
2020-03-26 11:14:46 +0300

    Mobile look is improved.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
-import '../Layout/Layout.css'
+import '../Layout/Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => {
-    return (
-        <Aux>
-            <div>
-                Toolbar, SideDrawer, Backdrop
-            </div>
-            <main className="Content">
-                {props.children}
-            </main>
-        </Aux>
-    )
+class Layout extends Component {
+
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !this.state.showSideDrawer }
+        });
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar
+                    drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <SideDrawer
+                    isOpen={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+                <main className="Content">
+                    {this.props.children}
+                </main>
+            </Aux>
+        )
+    }
 }
 
-export default layout;
+export default Layout;
